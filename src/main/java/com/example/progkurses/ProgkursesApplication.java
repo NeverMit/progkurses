ca450fe3e739c26eaa0ee1d37421b078ce27c51f
NeverMit <markovnikov96@mail.ru>
2024-01-29 18:12:07 +0500

    Initial commit

---
+++ src/main/java/com/example/progkurses/ProgkursesApplication.java
@@ -0,0 +1,13 @@
+package com.example.progkurses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgkursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgkursesApplication.class, args);
+	}
+
+}
