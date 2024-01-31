d804b7825cf67bd38d806d2e0f49dbd1f7e6f175
NeverMit <markovnikov96@mail.ru>
2024-01-31 20:04:29 +0500

    Подключили бд к приложению

--- src/main/java/com/example/progkurses/controllers/ProductController.java
+++ src/main/java/com/example/progkurses/controllers/ProductController.java
@@ -2,20 +2,22 @@ package com.example.progkurses.controllers;
 
 import com.example.progkurses.models.Product;
 import com.example.progkurses.services.ProductService;
+import freemarker.ext.beans.StringModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
     @GetMapping("/")
-    public String products(Model model){
-        model.addAttribute("products",productService.getListOfProducts());
+    public String products(@RequestParam(name="title",required = false) String title, Model model){
+        model.addAttribute("products",productService.listProducts(title));
         return "products";
     }
     @GetMapping("/product/{id}")
@@ -25,7 +27,7 @@ public class ProductController {
     }
     @PostMapping("/product/create")
     public String createProduct(Product product){
-        productService.saveProduct(product);
+        productService.addProduct(product);
         return "redirect:/";
     }
     @PostMapping("/product/delete/{id}")
--- src/main/java/com/example/progkurses/services/ProductService.java
+++ src/main/java/com/example/progkurses/services/ProductService.java
@@ -1,45 +1,31 @@
 package com.example.progkurses.services;
 
 import com.example.progkurses.models.Product;
+import com.example.progkurses.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
-    private List<Product> products=new ArrayList<>();
-    private long ID=0;
-    {
-        products.add(new Product(
-                ID++,
-                "Java Core",
-                "Курс по основам Java",
-                2000,
-                "Вася Пупкин")
-        );
-        products.add(new Product(
-                ID++,
-                "C++",
-                "Курс по основам C++",
-                1000,
-                "Азамат Шарипов")
-        );
+    private final ProductRepository productRepository;
+    public List<Product> listProducts(String title){
+        if(title!=null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
-    public List<Product> getListOfProducts(){
-        return products;
-    }
-    public void saveProduct(Product product){
-        product.setId(ID++);
-        products.add(product);
+    public void addProduct(Product product){
+        log.info("Adding new {}",product);
+        productRepository.save(product);
     }
     public void deleteProduct(Long id){
-        products.removeIf(product -> product.getId()==id);
+        productRepository.deleteById(id);
     }
     public Product getProductById(Long id){
-        for(Product product : products){
-            if(product.getId()==id) return product;
-        }
-        return null;
+       return productRepository.findById(id).orElse(null);
     }
 }
